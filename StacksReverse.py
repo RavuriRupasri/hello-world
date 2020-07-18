1ce9565836984e3511a8fe37a07eba9c9f95020d
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-18 15:29:29 +0530

    reversing stack

---
+++ StacksReverse.py
@@ -0,0 +1,27 @@
+class stack:
+    def __init__(self):
+        self.items=[]
+    def isempty(self):
+        return len(self.items)==0
+    def push(self,item):
+        self.items.append(item)
+    def pop(self):
+        if(isempty(self)):
+            print("Underflow condition")
+        return self.items.pop()
+    def top(self):
+        return self.items[-1]
+    def delstack(self):
+        self.items.pop(len(self.items)//2-1)
+        return self.items
+    def reverse(self):	
+		n=len(self.items)
+		for i in range((n//2)+1):
+			self.items[i],self.items[n-i-1]=self.items[n-i-1],self.items[i]
+		return self.items
+st=stack()
+for i in range(10):
+    st.push(str(i+1))
+print(st.delstack())
+print(st.reverse())
+print(st.top())

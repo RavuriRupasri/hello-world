bce905f216bd250d677434884c497d1e47a2c21a
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-18 16:52:13 +0530

    reversing a stack

--- StacksReverse.py
+++ StacksReverse.py
@@ -6,9 +6,12 @@ class stack:
     def push(self,item):
         self.items.append(item)
     def pop(self):
-        if(isempty(self)):
+        if self.isempty():
             print("Underflow condition")
-        return self.items.pop()
+        else:
+            n=len(self.items)
+            self.items=self.items[:n-1]
+            return self.items
     def top(self):
         return self.items[-1]
     def delstack(self):
@@ -19,9 +22,26 @@ class stack:
 		for i in range((n//2)+1):
 			self.items[i],self.items[n-i-1]=self.items[n-i-1],self.items[i]
 		return self.items
+    def reverseRecursive(self,start=0,end=0):
+        if end == 0:
+            end=len(self.items)-end-1
+        if(start > end):
+            return self.items 
+        else:
+            self.items[start], self.items[end]=self.items[end],self.items[start]
+            return self.reverseRecursive(start+1, end-1)
+        
+        
+        
 st=stack()
 for i in range(10):
     st.push(str(i+1))
+'''
 print(st.delstack())
 print(st.reverse())
 print(st.top())
+print(st.pop())
+print(st.pop())
+print(st.pop())
+'''
+print(st.reverseRecursive())

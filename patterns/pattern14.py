f269bed29ba7549dee4dc4c36a6811d03f936d67
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-22 22:02:24 +0530

    square problem

--- patterns/pattern14.py
+++ patterns/pattern14.py
@@ -1,7 +1,7 @@
 n = int(input("Enter the number of rows: "))
 for i in range(n):
     for j in range(n):
-        if(i == j or i > j):
+        if(j <= i):
             print(i+1,end = ' ')
         else:
             print(j+1,end = ' ')

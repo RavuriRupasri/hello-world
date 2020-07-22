35fb7967a7ed5594f35ca5d6978dac29c1d0d6a6
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-22 16:26:28 +0530

    linked list

--- stacks/linkedList.py
+++ stacks/linkedList.py
@@ -3,8 +3,8 @@ class Node:
         self.data=data
         self.next=None
 class LinkedList:
-    def __init__(self):
-        self.head=Node()
+    def __init__(self,data = None ):
+        self.head= Node(data)
     def append(self,item):
         newnode=Node(item)
         cur=self.head
@@ -14,18 +14,18 @@ class LinkedList:
     def display(self):
         cur=self.head
         st=''
-        while(cur.next != None):
-            cur = cur.next
+        while(cur != None):
             st = st + str(cur.data)
             if(cur.next != None):
                 st = st+ "->"
+            cur = cur.next
         print(st)
     def length(self):
-        count=0
+        count= 0
         cur = self.head
-        while(cur.next != None):
-            cur = cur.next
+        while(cur != None):
             count += 1 
+            cur = cur.next 
         return count
     def delete(self,index):
         if(index > self.length()):
@@ -36,7 +36,7 @@ class LinkedList:
         while (1):
             node = cur
             cur = cur.next
-            if ( i == index ):
+            if ( i == index-1 ):
                 node.next = cur.next
                 break
             i += 1 
@@ -49,19 +49,17 @@ class LinkedList:
         cur = self.head
         i = 0
         while(1):
-            cur = cur.next
             if ( i == index-1):
                 temp = cur.next
                 cur.next = node
                 node.next = temp
                 break
+            cur = cur.next
             i += 1
     def prepend(self,item):
         node = Node(item)
-        cur = self.head
-        temp = cur.next
-        cur.next = node
-        node.next = temp
+        node.next = self.head
+        self.head = node 
     def reverse(self):
         pre = None
         cur = self.head 
@@ -71,8 +69,7 @@ class LinkedList:
             pre = cur 
             cur = nex 
         self.head = pre 
-l=LinkedList()
-l.append(1)
+l = LinkedList(1) 
 l.append(2)
 l.append(3)
 l.display()
@@ -87,4 +84,5 @@ l.display()
 l.prepend(0)
 l.display()
 l.reverse()
-l.display()
+l.display()       
+print(l.length())

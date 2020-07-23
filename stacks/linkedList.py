123e84e392a8fabf674a8bb6b2902f45fc429b62
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-23 16:06:50 +0530

    linked list

--- stacks/linkedList.py
+++ stacks/linkedList.py
@@ -1,88 +1,140 @@
 class Node:
-    def __init__(self,data=None):
-        self.data=data
-        self.next=None
+    def __init__(self,data = None):
+        self.data = data
+        self.next = None
 class LinkedList:
-    def __init__(self,data = None ):
-        self.head= Node(data)
+    def __init__(self):
+        self.head = None
     def append(self,item):
-        newnode=Node(item)
-        cur=self.head
+        node = Node(item)
+        if self.head == None:
+            self.head = node 
+            return ;
+        cur = self.head
         while(cur.next != None):
-            cur=cur.next
-        cur.next=newnode
-    def display(self):
-        cur=self.head
-        st=''
-        while(cur != None):
-            st = st + str(cur.data)
-            if(cur.next != None):
-                st = st+ "->"
             cur = cur.next
-        print(st)
-    def length(self):
-        count= 0
-        cur = self.head
-        while(cur != None):
-            count += 1 
-            cur = cur.next 
-        return count
-    def delete(self,index):
-        if(index > self.length()):
-            print("Index range out of index")
-            return
-        i = 0
-        cur = self.head
-        while (1):
-            node = cur
-            cur = cur.next
-            if ( i == index-1 ):
-                node.next = cur.next
-                break
-            i += 1 
-    def insert(self,index,item):
-        if (index < 0):
-            print(" Item cannot be inserted. ")
-        if(index > self.length()):
-            print("Index is out of range. ")
+        cur.next = node
+    def prepend(self,item):
         node = Node(item)
         cur = self.head
-        i = 0
-        while(1):
-            if ( i == index-1):
+        node.next = cur
+        self.head = node 
+    def insert(self,index,item):
+        node = Node(item)
+        if(index < 0):
+            print("The value cannot be inserted.")
+            return ;
+        ind = 0
+        cur = self.head
+        while(cur.next != None):
+            if(ind == index):
                 temp = cur.next
                 cur.next = node
                 node.next = temp
+            cur = cur.next
+            ind += 1
+    def length(self):
+        count = 0
+        cur = self.head
+        while(cur != None):
+            count += 1 
+            cur = cur.next
+        print(count)
+    def length_recursive(self,cur = 0):
+        if(cur == 0):
+            cur = self.head
+        if(cur == None):
+            return 0;
+        else:
+            return 1+self.length_recursive(cur.next);
+    def delete(self,index):
+        ind = 0
+        cur = self.head
+        while(cur.next != None):
+            if(ind == index-1):
+                temp = cur.next
+                cur.next = temp.next
+                break
+            ind += 1 
+            cur = cur.next
+    def delete_item(self,item):
+        cur = self.head
+        while(cur.next != None):
+            if(cur.next.data == item):
+                temp = cur.next
+                cur.next = temp.next
                 break
             cur = cur.next
-            i += 1
-    def prepend(self,item):
-        node = Node(item)
-        node.next = self.head
-        self.head = node 
     def reverse(self):
-        pre = None
+        prev = None
         cur = self.head 
         while(cur != None):
-            nex = cur.next
-            cur.next = pre
-            pre = cur 
-            cur = nex 
-        self.head = pre 
-l = LinkedList(1) 
+            next  = cur.next
+            cur.next = prev
+            prev = cur 
+            cur = next 
+        self.head = prev
+    def search(self,item):
+        ind =0
+        cur = self.head
+        while(cur != None):
+            if(cur.data == item):
+                print("The element is present at position : ",ind+1 )
+                return ;
+            ind += 1 
+            cur = cur.next
+        else:
+            print("The element is not found in the Linked List.")
+    def search_recursive(self,item,cur = 0):
+        if(cur == 0):
+            cur = self.head
+        if(cur == None):
+            return False;
+        if(cur.data == item):
+            return True;
+        else:
+            return self.search_recursive(item,cur.next);
+    def display(self):
+        cur = self.head
+        st = ''
+        if(self.head == None):
+            print("Empty linked list. ")
+            return;
+        while(cur != None):
+            st = st + str(cur.data)
+            if(cur.next != None):
+                st = st + '->'
+            cur = cur.next
+        print(st)
+    def getNthNode(self,index):
+        cur = self.head
+        ind = 0
+        while(cur != None):
+            if(ind == index):
+                print(cur.data)
+                return;
+            ind += 1 
+            cur = cur.next
+        print("Index out of range. ")
+    def getNth_recursive(self,ind,cur = 0):
+        i = 0
+        if(cur ==0 ):
+            cur = self.head
+        if(i == ind):
+            return cur.data;
+        else:
+            return self.getNth_recursive(ind-1,cur.next)
+l = LinkedList()
+l.append(1)
 l.append(2)
 l.append(3)
-l.display()
-l.delete(1)
-l.display()
 l.append(4)
-l.append(5)
-l.insert(2,2)
-l.display()
-l.append(4)
-l.display()
-l.prepend(0)
+l.insert(1,6)
+l.delete_item(3)
+l.search(25)
 l.display()
 l.reverse()
-l.display()       
-print(l.length())
+l.display()
+print(l.length_recursive())
+print(l.search_recursive(1))
+print(l.getNth_recursive(2))

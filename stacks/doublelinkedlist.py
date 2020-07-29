52a8eb9563b0cbc32f9fccf5692bc690a727bbde
RavuriRupasri <rupasri.ravuri@gmail.com>
2020-07-29 12:00:42 +0530

    Double Linked List

---
+++ stacks/doublelinkedlist.py
@@ -0,0 +1,37 @@
+class Node:
+	def __init__(self,data = None):
+		self.data = data 
+		self.next = None
+		self.prev = None
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self,item):
+        node = Node(item)
+        if(self.head == None):
+            self.head = node
+            node.next = None
+            return 
+        cur = self.head
+        while(cur.next != None):
+            cur = cur.next
+        cur.next = node 
+        node.prev = cur
+    def display(self):
+        cur = self.head
+        print("Forward direction\n")
+        while(cur.next != None):
+            print "%d" %cur.data,
+            last = cur
+            cur = cur.next
+        print("\nReverse direction\n")
+        while(last != None):
+            print "%d" %last.data,
+            last = last.prev
+dl = DoubleLinkedList()
+dl.append(1)
+dl.append(2)
+dl.append(3)
+dl.append(4)
+dl.append(5)
+dl.display()
